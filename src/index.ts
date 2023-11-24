ddca02e5f6a0669e8ebbca5bddc275682f98406e
Wachira-Macharia <eugenecomboni@gmail.com>
2023-11-24 14:49:12 +0300

    Authentication Routing and Logging in

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "./router";
 
 const app = express();
 
@@ -29,5 +30,6 @@ const MONGO_URL =
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-
 mongoose.connection.on("error", (error: Error) => console.log(Error));
+
+app.use("/", router());
